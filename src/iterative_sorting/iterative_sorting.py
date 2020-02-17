af98d10c5960d58b3ef3af116f7808a6a14f203c
Timothy Sharkey <Techne3@gmail.com>
2020-02-17 14:58:43 -0700

    bubble sort completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,16 @@
 # TO-DO: Complete the selection_sort() function below 
+
+'''
+The selection sort algorithm sorts an array by repeatedly finding the minimum element (considering ascending order) from unsorted part and putting it at the beginning. The algorithm maintains two subarrays in a given array.
+
+1) The subarray which is already sorted.
+2) Remaining subarray which is unsorted.
+
+In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted subarray is picked and moved to the sorted subarray.
+
+'''
+
+
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
@@ -24,7 +36,27 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    bubble_len = len(arr)
+    for i in range(len(arr)):
+        for j in range(0, bubble_len-i-1):
+            if arr[j] > arr[j+1]:
+                #swap if the element found is greater
+                # than the next element
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
+
+def bubble_sort(arr):
+    has_swapped = True
+    while has_swapped:
+        has_swapped = False
+        # iterate through the array and see if any swaps have ocurred 
+        for i in range(0, len(arr)-1):
+            # if the element is larger, swap it to the right
+            if arr[i] > arr[i + 1]:
+                arr[i + 1], arr[i] = arr[i], arr[i + 1]
+                has_swapped = True
+                break
     return arr
 
 
